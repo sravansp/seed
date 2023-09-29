79bfd337b6d73794f7944edf8a875a064c56e4c8
Sravan SP <sravansp92@gmail.com>
2023-09-29 18:14:31 +0530

    change in intake

--- assets/js/index.js
+++ assets/js/index.js
@@ -296,3 +296,21 @@ $(document).ready(function () {
         label.attr("for", chekBoxId);
     }
 });
+
+function printtag(tagid) {
+    var hashid = "#"+ tagid;
+    // var tagname =  $(hashid).prop("tagName").toLowerCase() ;
+    var attributes = ""; 
+    var attrs = document.getElementById(tagid).attributes;
+      $.each(attrs,function(i,elem){
+        attributes +=  " "+  elem.name+" ='"+elem.value+"' " ;
+      })
+    var divToPrint= $(hashid).html() ;
+    var head = "<html><head>"+ $("head").html() + "</head>" ;
+    var allcontent = head + "<body  onload='window.print()' >"+  divToPrint +  "</body></html>"  ;
+    var newWin=window.open('','Print-Window');
+    newWin.document.open();
+    newWin.document.write(allcontent);
+    newWin.document.close();
+   // setTimeout(function(){newWin.close();},10);
+}
