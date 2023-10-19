ba286de0d3bcb0b65b8865dc63d5bd3f40005d50
Sravan SP <sravansp92@gmail.com>
2023-10-19 17:32:47 +0530

    bug fix

--- assets/js/calender.js
+++ assets/js/calender.js
@@ -14,14 +14,19 @@ function initializeCalendar() {
     selectMonth = document.getElementById("month");
 
     createYear = generate_year_range(1970, 2050);
+    /** or
+     * createYear = generate_year_range( 1970, currentYear );
+     */
     if (selectYear) {
         document.getElementById("year").innerHTML = createYear;
     }
 
     var calendar = document.getElementById("calendar");
+    // var lang = calendar.getAttribute("data-lang");
 
     var months = "";
     var days = "";
+
 
     // Add a "Today" button click event listener
     const todayButton = document.getElementById("todayButton");
@@ -37,6 +42,22 @@ function initializeCalendar() {
         {
             date: "2023-10-11",
             title: "Meeting with friends",
+        },
+        {
+            date: "2023-10-16",
+            title: "Team Lunch",
+        },
+        {
+            date: "2023-10-18",
+            title: "Presentation",
+        },
+        {
+            date: "2023-10-15",
+            title: "Another Meeting",
+        },
+        {
+            date: "2023-10-18",
+            title: "Presentation",
         },
         // Add more events as needed
     ];
@@ -58,8 +79,13 @@ function initializeCalendar() {
 
     var dayDefault = ["S", "M", "T", "W", "T", "F", "S"];
 
+    // if (lang == "en") {
     months = monthDefault;
     days = dayDefault;
+    // } else {
+    // months = monthDefault;
+    // days = dayDefault;
+    // }
 
     var $dataHead = "<tr>";
     for (dhead in days) {
@@ -67,48 +93,34 @@ function initializeCalendar() {
     }
     $dataHead += "</tr>";
 
+    //alert($dataHead);
     document.getElementById("thead-month").innerHTML = $dataHead;
 
     monthAndYear = document.getElementById("monthAndYear");
+    
     showCalendar(currentMonth, currentYear);
 
-    function next() {
-        currentYear = currentMonth === 11 ? currentYear + 1 : currentYear;
-        currentMonth = (currentMonth + 1) % 12;
-        updateSelectBoxes();
-        showCalendar(currentMonth, currentYear);
-    }
-
-    function previous() {
-        currentYear = currentMonth === 0 ? currentYear - 1 : currentYear;
-        currentMonth = currentMonth === 0 ? 11 : currentMonth - 1;
-        updateSelectBoxes();
-        showCalendar(currentMonth, currentYear);
-    }
-
-    function jump() {
-        currentYear = parseInt(selectYear.value);
-        currentMonth = parseInt(selectMonth.value);
-        showCalendar(currentMonth, currentYear);
-    }
-
-    // Make the showCalendar function available in the global scope
     window.showCalendar = showCalendar;
-
 
     function showCalendar(month, year) {
         var firstDay = new Date(year, month).getDay();
+
         tbl = document.getElementById("calendar-body");
+
         tbl.innerHTML = "";
 
         monthAndYear.innerHTML = months[month] + " " + year;
         selectYear.value = year;
         selectMonth.value = month;
 
+
+        // Function to count events for a given date
         function countEventsForDate(targetDate) {
             return events.filter((event) => event.date === targetDate).length;
         }
 
+
+        // creating all cells
         var date = 1;
         for (var i = 0; i < 6; i++) {
             var row = document.createElement("tr");
@@ -128,7 +140,7 @@ function initializeCalendar() {
                     cell.setAttribute("data-year", year);
                     cell.setAttribute("data-month_name", months[month]);
                     cell.className = "date-picker";
-                    cell.innerHTML = "<a href='#'>" + date + "</a";
+                    cell.innerHTML = "<a href='#'>" + date + "</a>";
 
                     if (
                         date === today.getDate() &&
@@ -138,9 +150,13 @@ function initializeCalendar() {
                         cell.className = "date-picker selected";
                     }
 
+                    // Calculate the date string for the current cell
                     const dateString = `${year}-${month + 1}-${date}`;
+
+                    // Find the count of events for the current date
                     const eventCount = countEventsForDate(dateString);
 
+                    // Display the event count if it's greater than 0
                     if (eventCount > 0) {
                         const eventCountElement = document.createElement("div");
                         eventCountElement.className = "event-count";
@@ -149,6 +165,7 @@ function initializeCalendar() {
                         eventCountElement.textContent = eventCount;
                         cell.appendChild(eventCountElement);
                     }
+
 
                     row.appendChild(cell);
                     date++;
@@ -159,45 +176,58 @@ function initializeCalendar() {
         }
     }
 
+
+
     function daysInMonth(iMonth, iYear) {
         return 32 - new Date(iYear, iMonth, 32).getDate();
     }
 
-    function updateSelectBoxes() {
-        selectYear.value = currentYear;
-        selectMonth.value = currentMonth;
-    }
-
-    function jumpToToday() {
-        const today = new Date();
-        currentYear = today.getFullYear();
-        currentMonth = today.getMonth();
-        showCalendar(currentMonth, currentYear);
-        updateSelectBoxes();
-    }
-
     if (document.querySelector("#date")) {
+        // Get today's date
         const todays = new Date();
+
+        // Format the date as "01/01/2023"
         const formattedDate = `${todays.getDate().toString().padStart(2, '0')}/${(todays.getMonth() + 1).toString().padStart(2, '0')}/${todays.getFullYear()}`;
+
+        // Display the formatted date in the specified <span> element
         document.getElementById('date').textContent = formattedDate;
     }
 
     if (document.querySelector("#today")) {
+
         const todaydate = new Date();
+
+        // Get today's date
         const day = today.getDate();
+
+        // Get the day of the week (0 = Sunday, 1 = Monday, ..., 6 = Saturday)
         const dayOfWeek = today.getDay();
+
+        // Define an array of day names
         const dayNames = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+
+        // Get the month (0 = January, 1 = February, ..., 11 = December)
         const month = today.getMonth();
+
+        // Define an array of month names
         const monthNames = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
+
+        // Get the year
         const year = today.getFullYear();
+
+        // Get the day of the week name
         const dayOfWeekName = dayNames[dayOfWeek];
 
+        // Display the results
         document.getElementById("today").textContent = day;
         document.getElementById("dayOfWeek").textContent = dayOfWeekName;
         document.getElementById("today-month").textContent = monthNames[month];
         document.getElementById("today-year").textContent = year;
+
     }
 }
+
+
 
 // Call the function to initialize the calendar
 initializeCalendar();
@@ -221,6 +251,10 @@ function jump() {
 function updateSelectBoxes() {
     selectYear.value = currentYear;
     selectMonth.value = currentMonth;
+
+    // Trigger the change event for the Select2 select boxes
+    $(selectYear).trigger('change.select2');
+    $(selectMonth).trigger('change.select2');
 }
 
 function jumpToToday() {
